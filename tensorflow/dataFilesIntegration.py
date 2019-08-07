2d29e294574ea28ad273e126bf556cb24d03720d
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2019-08-07 14:03:07 +0530

    Integration file

--- tensorflow/dataFilesIntegration.py
+++ tensorflow/dataFilesIntegration.py
@@ -5,19 +5,14 @@ import pandas as pd
 path = 'D:/FYP/ds'
 os.chdir(path)
 
-allFiles = os.listdir()
+allFiles = glob.glob(path + "/*.CSV")
 
 print(allFiles)
 
-fileList = [filename for filename in os.listdir('.')]
-print(fileList)
-
-for filename in selectedFiles:
-    print(filename)
-    #combine all files in the list
-    combined_csv = pd.concat([pd.read_csv(filename, delimiter = "\t") for filename in selectedFiles])
-    print(combined_csv)
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(filename, delimiter = "\t") for filename in allFiles])
+print(combined_csv)
 
 
 ##export to csv
-#combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')

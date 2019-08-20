3da2b2a6d4c91574f4ec12924cd5d5c2101d600d
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-08-20 16:59:09 +0530

    Changes to Cleaning main.go

--- framework/workflow/cleaning/dropUserDefinedRows.py
+++ framework/workflow/cleaning/dropUserDefinedRows.py
@@ -1,21 +1,24 @@
 # importing pandas module 
 import pandas as pd 
-url = "https://raw.githubusercontent.com/dataoptimal/posts/master/data%20cleaning%20with%20python%20and%20pandas/property%20data.csv"
+import sys
+
+#url = "https://raw.githubusercontent.com/dataoptimal/posts/master/data%20cleaning%20with%20python%20and%20pandas/property%20data.csv"
   
 # making data frame from csv file 
 #drop values from index label
 
-df = pd.read_csv(url ) 
-columnName = 'NUM_BATH'
-deleteValues = ["1","2"]
+df = pd.read_csv(sys.argv[1] ) 
+columnName = sys.argv[2]
+deleteValues = [sys.argv[3], sys.argv[4]]
 
 for i in deleteValues:
-  df_filtered = df[df[columnName] == i]
+
+  dfUserDroppedRows = df[df[columnName] != i]
   
 
   
 # display 
-df_filtered
+#dfUserDroppedRows
 
 # Delete the rows with label "Ireland"
 # For label-based deletion, set the index first on the dataframe:
@@ -23,3 +26,4 @@ df_filtered
 #df = df.drop(["BERKELEY", "PUTNAM"], axis=0) # Delete all rows with label "Ireland"
 
 #df
+dfUserDroppedRows.to_csv (r'/home/amanda/FYP/testcsv/cleanedDataset.csv', index = None, header=True)
--- framework/workflow/cleaning/removeDuplicateRows.py
+++ framework/workflow/cleaning/removeDuplicateRows.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
+import sys
 
 #user defined missing values
 missing_values = ["n/a", "na", "--"]
 
 #read csv with defined missing values 
-url = "https://raw.githubusercontent.com/dataoptimal/posts/master/data%20cleaning%20with%20python%20and%20pandas/property%20data.csv"
-df = pd.read_csv(url, na_values = missing_values)
+#url = "https://raw.githubusercontent.com/dataoptimal/posts/master/data%20cleaning%20with%20python%20and%20pandas/property%20data.csv"
+df = pd.read_csv(sys.argv[1], na_values = missing_values)
 
-print(df)
+#print(df)
 
 dfDroppedDuplicates = df.drop_duplicates()
 dfDroppedDuplicates.reset_index(inplace=True)
 
-print(dfDroppedDuplicates)
+dfDroppedDuplicates.to_csv (r'/home/amanda/FYP/testcsv/cleanedDataset.csv', index = None, header=True)
+
+#print(dfDroppedDuplicates)

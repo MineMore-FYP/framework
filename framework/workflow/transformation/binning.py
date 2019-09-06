a0c533c7427b15a69e59f5befe0a89448b2683db
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-09-06 14:41:08 +0530

    transformation modules

--- framework/workflow/transformation/binning.py
+++ framework/workflow/transformation/binning.py
@@ -1,22 +1,25 @@
 #Binning for integer features:
 import pandas as pd
 import numpy as np
+sys.path.append("..")
+from dataType import *
+from userScript import *
 
-url = "https://raw.githubusercontent.com/dataoptimal/posts/master/data%20cleaning%20with%20python%20and%20pandas/property%20data.csv"
- 
-data = pd.read_csv(url)
-colName = "ST_NUM"
-df_no_missing = data[colName].dropna()
-binArray = np.array([])
+df = pd.read_csv(outputDataset)
+columnNames = userDefinedBinningColumns
 
-for row in df_no_missing:
-   intValue = int(row)
-   binArray = np.append(binArray, intValue)
-  
-binArray[0]  
-#pd.cut(np.array([.2, 1.4, 2.5, 6.2, 9.7, 2.1]), 3, retbins=True)
-#pd.cut(np.ones(5), 4, labels=False)
+for col in columnNames:
+    #df_no_missing = data[colName].dropna()
+    binArray = np.array([])
 
-pd.cut(binArray, 3, labels=["0", "1","2"])
+    for row in df_no_missing:
+       intValue = int(row)
+       binArray = np.append(binArray, intValue)
 
-#remove 'dropna' from code. already done by this stage.
+    binArray[0]
+    #pd.cut(np.array([.2, 1.4, 2.5, 6.2, 9.7, 2.1]), 3, retbins=True)
+    #pd.cut(np.ones(5), 4, labels=False)
+
+    pd.cut(binArray, 3, labels=["0", "1","2"])
+
+    #drop na already done by this stage.

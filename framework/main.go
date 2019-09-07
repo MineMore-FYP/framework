e3329ed49a91f1d654a2fbc6ed36a61f266280a7
amandaw95 <amanda_wijewickrama@yahoo.com>
2019-09-07 17:43:52 +0530

    Resolve some errors in workflow

--- framework/main.go
+++ framework/main.go
@@ -31,21 +31,21 @@ func pythonCall(progName string, dataset string){
 func main() {
 	//get input dataset location
 	cmd := exec.Command("python",  "-c", "from workflow import userScript; print userScript.inputDataset")
-	fmt.Println(cmd.Args)
+	//fmt.Println(cmd.Args)
 	out, err := cmd.CombinedOutput()
 	if err != nil { fmt.Println(err); }
 	//input dataset from disk
 	inputDataset := string(out)[:len(out)-1]
-	fmt.Print(inputDataset)
+	//fmt.Print(inputDataset)
 
 	//get output dataset location
 	cmd1 := exec.Command("python",  "-c", "from workflow import userScript; print userScript.outputDataset")
-	fmt.Println(cmd1.Args)
+	//fmt.Println(cmd1.Args)
 	out1, err1 := cmd1.CombinedOutput()
 	if err1 != nil { fmt.Println(err1); }
 	//input dataset from disk
 	outputDataset := string(out1)[:len(out1)-1]
-	fmt.Print(outputDataset)
+	//fmt.Print(outputDataset)
 
 ///////////////////////////*****************SELECTION************************////////////////////////
 
@@ -58,7 +58,7 @@ func main() {
 	defer close(channel)
   go SendValue(outputDataset, channel)
   output := <-channel
-  fmt.Println(output)
+  fmt.Println("Select User Defined Columns Complete")
   time.Sleep(10000 * time.Millisecond)
 
 ///////////////////////////*****************CLEANING************************////////////////////////
@@ -73,7 +73,7 @@ func main() {
 	defer close(channel1)
   go SendValue(outputDataset, channel1)
   output1 := <-channel1
-  fmt.Println(output1)
+  fmt.Println("Drop unique columns complete")
   time.Sleep(10000 * time.Millisecond)
 
   //drop one value cols
@@ -85,7 +85,7 @@ func main() {
   defer close(channel2)
   go SendValue(outputDataset, channel2)
   output2 := <-channel2
-  fmt.Println(output2)
+  fmt.Println("Drop one value columns complete")
   time.Sleep(10000 * time.Millisecond)
 
   //Drop user defined cols
@@ -97,31 +97,31 @@ func main() {
   defer close(channel3)
   go SendValue(outputDataset, channel3)
   output3 := <-channel3
-  fmt.Println(output3)
+  fmt.Println("Drop user defined columns complete")
   time.Sleep(10000 * time.Millisecond)
 
   //#drop columns according to user defined empty value percentage
   go pythonCall("workflow/cleaning/dropColumnsCriteria.py", output3)
   time.Sleep(10000 * time.Millisecond)
 
-  //channel
+  /*//channel
   channel4 := make(chan string)
   defer close(channel4)
   go SendValue(outputDataset, channel4)
   output4 := <-channel4
-  fmt.Println(output4)
+  fmt.Println("Drop columns criteria complete")
   time.Sleep(10000 * time.Millisecond)
 
   //#drop user defined rows
   go pythonCall("workflow/cleaning/dropUserDefinedRows.py", output4)
   time.Sleep(10000 * time.Millisecond)
-
+*/
   //channel
   channel5 := make(chan string)
   defer close(channel5)
   go SendValue(outputDataset, channel5)
   output5 := <-channel5
-  fmt.Println(output5)
+  fmt.Println("Drop user defined rows complete")
   time.Sleep(10000 * time.Millisecond)
 
   //drop rows according to user defined empty value percentage
@@ -133,7 +133,7 @@ func main() {
   defer close(channel6)
   go SendValue(outputDataset, channel6)
   output6 := <-channel6
-  fmt.Println(output6)
+  fmt.Println("Drop row criteria complete")
   time.Sleep(10000 * time.Millisecond)
 
   //#remove duplicate rows
@@ -145,7 +145,7 @@ func main() {
   defer close(channel7)
   go SendValue(outputDataset, channel7)
   output7 := <-channel7
-  fmt.Println(output7)
+  fmt.Println("Remove duplicate rows complete")
   time.Sleep(10000 * time.Millisecond)
 
   //missing value interpolation
@@ -157,13 +157,15 @@ func main() {
   defer close(channel8)
   go SendValue(outputDataset, channel8)
   output8 := <-channel8
-  fmt.Println(output8)
+  fmt.Println("Missing values interpolate complete")
   time.Sleep(10000 * time.Millisecond)
 
   //mode for user defined columns
   go pythonCall("workflow/cleaning/missingValuesMode.py", output8)
   time.Sleep(10000 * time.Millisecond)
 
+  fmt.Println("Missing values mode complete")
+  
 
 }
 
